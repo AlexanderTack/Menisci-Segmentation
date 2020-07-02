cce9bdf59b4db7d1bf8cfc6cc28945359d2b25ba
AlexanderTack <36989276+AlexanderTack@users.noreply.github.com>
2020-07-02 14:33:08 +0200

    Create 03_segment_3D_meniscus.py

---
+++ 2-step_approach_2D_U-Net__3D_U-Net/03_segment_3D_meniscus.py
@@ -0,0 +1,24 @@
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+#
+# This program is segmenting the menisci from 3D waterexcited dual-echo steady-state MRIs.
+#
+# The code is loosely based on the code developed by Marko Jocic and uploaded on github (https://github.com/jocicmarko/ultrasound-nerve-segmentation).
+# Marko Jocic originally developed the code for the ultrasound segmentation challenge (https://www.kaggle.com/c/ultrasound-nerve-segmentation, MIT license).
+#
+# Copyright (C) 2017 Alexander Tack, Felix Ambellan
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+#
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------
